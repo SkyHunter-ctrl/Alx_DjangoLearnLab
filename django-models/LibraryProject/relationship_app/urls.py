77644213da1f3eb8ca2af4986551a708e3fad5e2
SkyHunter <ledemas09@gmail.com>
2025-09-06 12:38:46 +0100

    changes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,6 @@
 # Configure URL Patterns
 from django.urls.py import path
+from .views import list_books, LibraryDetailView, hello_view
 from .import views
 urlpatterns = [
     path('hello/', views.hello_view, name='hello'),
