60ec000647d025f7a44835de283fa6b3aabb5402
SkyHunter <ledemas09@gmail.com>
2025-09-06 11:03:53 +0100

    changes

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,14 +5,16 @@ def book_list(request):
     return render(request, 'relationship_app/list_books.html', context={'books': books})
 # Class-based View to show book details
 from django.views.generic import DetailView 
+from .models import Library
 class BookDetailView(DetailView):
-    model = Book
-    template_name = 'book_detail.html'
+    model = Library
+    library = 'relationship_app/library_detail.html'
     context_object_name = 'book'
 # the hello_view function takes an HTTP request (request) as input and returns an HTTP response containing the string “Hello, World!”.
 from django.http import HttpResponse
 def hello_view(request):
     return HttpResponse("Hello, World!")
+#
 
 
     
