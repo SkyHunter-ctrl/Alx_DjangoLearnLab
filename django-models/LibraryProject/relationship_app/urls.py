1dc4bd307ead04e3ff9bf3c9646cd1ee7fe31166
SkyHunter <ledemas09@gmail.com>
2025-09-07 21:36:33 +0100

    min

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -40,8 +40,9 @@ urlpatterns = [
 ]
 # Task 3. Implement Role-Based Access Control in Django
 from django.urls import path
-from .views import admin_view, librarian_view, member_view
-
+from .views import admin_view
+from .views import librarian_view
+from .views import member_view
 urlpatterns = [
     path('admin-role/', admin_view, name='admin_view'),
     path('librarian-role/', librarian_view, name='librarian_view'),
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -90,16 +90,22 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import user_passes_test
 #class UserProfile(models.Model):
 
-#    ROLE_CHOICES = [
-#        ('Admin', 'Admin'),
-#        ('Librarian', 'Librarian'),
-#        ('Member', 'Member'),
-#    ]
-#    user = models.OneToOneField(User, on_delete=models.CASCADE)
-#    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+# userprofile model check 
+from django.db import models
+from django.contrib.auth.models import User
 
-#    def __str__(self):
-#        return f"{self.user.username} ({self.role})"
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} ({self.role})"
+    
 # Role-Based Views
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
