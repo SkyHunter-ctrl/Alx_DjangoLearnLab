6551ce6f4c7a05504a9355f7c540abc2be4406d8
SkyHunter <ledemas09@gmail.com>
2025-09-10 16:37:12 +0100

    task 3

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -30,3 +30,12 @@ urlpatterns = [
     # Class-based logout view
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
+#Task 3: config 
+from django.urls import path
+from .views import admin_view, librarian_view, member_view
+
+urlpatterns += [
+    path('admin-role/', admin_view, name='admin_view'),
+    path('librarian-role/', librarian_view, name='librarian_view'),
+    path('member-role/', member_view, name='member_view'),
+]
