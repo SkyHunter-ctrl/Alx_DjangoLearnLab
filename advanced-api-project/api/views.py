f6b03b38bdae7a38b8eb245fcc2346eda1886f72
SkyHunter <ledemas09@gmail.com>
2025-09-27 09:37:16 +0100

    change of name

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -36,6 +36,7 @@ class BookDeleteView(generics.DestroyAPIView):
 
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
