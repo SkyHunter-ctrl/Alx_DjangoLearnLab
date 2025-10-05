903db03ec86d8e40fbecf05e47751c25f5e868fd
SkyHunter <ledemas09@gmail.com>
2025-10-05 15:05:20 +0100

    auth

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import views as auth_views
 from . import views
+from django.urls import path
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
